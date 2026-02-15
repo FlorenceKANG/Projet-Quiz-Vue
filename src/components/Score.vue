09d1f800fc3712ff2f3c5ed3796df2f3d95a79c4
Florence KANG <fkang@outlook.fr>
2026-02-15 19:19:31 +0100

    feat: added score component

---
+++ src/components/Score.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  quiz: Object,
+  answers: Array,
+});
+
+const score = computed(() => {
+  return props.quiz.questions.reduce((acc, question, k) => {
+    if (question.correct_option_uuid === props.answers[k]) {
+      return acc + 1;
+    }
+
+    return acc;
+  }, 0);
+});
+</script>
+
+<template>
+  <div class="absolute center">
+    <h3>Score</h3>
+    <div class="shape sided-cookie12 extra">
+      <p>{{ score }} / {{ quiz.questions.length }}</p>
+    </div>
+  </div>
+</template>
