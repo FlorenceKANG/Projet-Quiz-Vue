57395b535c1e016ff139a65e1eb9e9a189ccfcd0
Florence KANG <fkang@outlook.fr>
2026-02-15 13:31:09 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <header>
+    <img src="./assets/logo.png" alt="logo du site" />
+    <h1>Vue Quiz</h1>
+  </header>
+  <div></div>
+</template>
+
+<style>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+}
+</style>
