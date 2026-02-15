745fdc9a45bc56d4374e98a36ac2e94256824a09
Florence KANG <fkang@outlook.fr>
2026-02-15 17:50:28 +0100

    refactor: separate quiz card component

---
+++ src/components/QuizCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useRouter } from "vue-router";
+
+defineProps({ quiz: Object });
+
+const router = useRouter();
+</script>
+
+<template>
+  <article class="no-padding">
+    <div class="grid no-space">
+      <div class="s3">
+        <img class="responsive" :src="quiz.image_url" />
+      </div>
+      <div class="s6">
+        <div class="padding">
+          <h5>{{ quiz.title }}</h5>
+          <p>
+            {{ quiz.description }}
+          </p>
+          <nav>
+            <button class="round" @click="router.push(`/quiz/${quiz.uuid}`)">
+              Commencer
+            </button>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import ErrorState from "@/components/ErrorState.vue";
+import QuizCard from "@/components/QuizCard.vue";
 import Spinner from "@/components/Spinner.vue";
 import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
@@ -7,8 +8,6 @@ import { useRouter } from "vue-router";
 const quizzes = ref([]);
 const state = ref("idle");
 const errorMessage = ref("");
-
-const router = useRouter();
 
 onMounted(async () => {
   state.value = "loading";
@@ -37,25 +36,6 @@ onMounted(async () => {
   <Spinner v-if="state === 'loading'" />
   <ErrorState v-else-if="state === 'error'" :error-message="errorMessage" />
   <div v-else-if="state === 'success'">
-    <article v-for="quiz in quizzes" :key="quiz.uuid" class="no-padding">
-      <div class="grid no-space">
-        <div class="s3">
-          <img class="responsive" :src="quiz.image_url" />
-        </div>
-        <div class="s6">
-          <div class="padding">
-            <h5>{{ quiz.title }}</h5>
-            <p>
-              {{ quiz.description }}
-            </p>
-            <nav>
-              <button class="round" @click="router.push(`/quiz/${quiz.uuid}`)">
-                Commencer
-              </button>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </article>
+    <QuizCard v-for="quiz in quizzes" :key="quiz.uuid" :quiz="quiz" />
   </div>
 </template>
