a86ba4a92ab1f593cb4392a12f4276cfa44309e2
Florence KANG <fkang@outlook.fr>
2026-02-15 22:58:23 +0100

    feat: change theme color and switch light or dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,47 @@
+<script setup>
+import { computed, ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const state = ref("light");
+
+const isDark = computed(() => state.value === "dark");
+
+const onToggleMode = (e) => {
+  const mode = e.target.checked ? "dark" : "light";
+  ui("mode", mode);
+  state.value = mode;
+};
+</script>
+
 <template>
   <header>
-    <img src="./assets/logo.png" alt="logo du site" width="100" height="100" />
-    <h1>Vue Quiz</h1>
+    <div class="row">
+      <img
+        src="./assets/logo.png"
+        alt="logo du site"
+        width="100"
+        height="100"
+      />
+      <h1>Vue Quiz</h1>
+    </div>
+    <nav>
+      <button class="transparent square" @click="router.push('/')">
+        <i>home</i>
+      </button>
+      <label class="switch icon">
+        <input
+          type="checkbox"
+          v-model="state"
+          :checked="isDark"
+          @change="onToggleMode"
+        />
+        <span>
+          <i>{{ isDark ? "bedtime" : "routine" }}</i>
+        </span>
+      </label>
+    </nav>
   </header>
   <main>
     <RouterView />
@@ -11,8 +51,9 @@
 <style>
 header {
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   gap: 1rem;
   padding: 1rem !important;
 }
--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+await ui("theme", "#8b5000");
+
 createApp(App).use(router).mount("#app");
