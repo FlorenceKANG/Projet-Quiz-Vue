7ccf2114e12cd4050f89eb149254742fde0a31bb
Florence KANG <fkang@outlook.fr>
2026-02-15 15:11:37 +0100

    feat: fetch quiz data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,70 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+
+const quizzes = ref([]);
+const state = ref("idle");
+const errorMessage = ref("");
+
+onMounted(async () => {
+  state.value = "loading";
+  try {
+    const res = await fetch("/data/quizzes.json");
+
+    if (!res.ok) {
+      throw new Error("Impossible de récupérer les données.");
+    }
+    const data = await res.json();
+
+    quizzes.value = data.quiz_collection;
+    state.value = "success";
+  } catch (error) {
+    console.error(error);
+    state.value = "error";
+    errorMessage.value =
+      "Une erreur s'est produite lors de la récupération des données. Veuillez réessayer plus tard.";
+  }
+});
+</script>
 
 <template>
   <header>
     <img src="./assets/logo.png" alt="logo du site" />
     <h1>Vue Quiz</h1>
   </header>
-  <div></div>
+  <div class="container">
+    <div v-if="state === 'loading'" class="middle-align center-align">
+      <progress class="circle wavy indeterminate" value="50" max="100" />
+    </div>
+    <article v-if="state === 'error'" class="middle-align center-align">
+      <div>
+        <div class="row middle-align center-align">
+          <i class="extra">error</i>
+          <h5>Erreur produite</h5>
+        </div>
+        <p>{{ errorMessage }}</p>
+      </div>
+    </article>
+    <div v-if="state === 'success'">
+      <article v-for="quiz in quizzes" :key="quiz.uuid" class="no-padding">
+        <div class="grid no-space">
+          <div class="s3">
+            <img class="responsive" :src="quiz.image_url" />
+          </div>
+          <div class="s6">
+            <div class="padding">
+              <h5>{{ quiz.title }}</h5>
+              <p>
+                {{ quiz.description }}
+              </p>
+              <nav>
+                <button class="round">Commencer</button>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
+  </div>
 </template>
 
 <style>
@@ -14,5 +73,9 @@ header {
   align-items: center;
   justify-content: center;
   gap: 1rem;
+  padding: 1rem;
+}
+.container {
+  padding: 1rem;
 }
 </style>
